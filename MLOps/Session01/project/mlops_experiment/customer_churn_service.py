63fff65c0d73a5a3bba34a907ed52f0b6e42c2f6
AIPQDev <84112281+AIPQDev@users.noreply.github.com>
2021-06-19 11:02:02 +0530

    update
    update

---
+++ MLOps/Session01/project/mlops_experiment/customer_churn_service.py
@@ -0,0 +1,18 @@
+import pandas as pd
+import bentoml
+from bentoml.frameworks.sklearn import SklearnModelArtifact
+from bentoml.service.artifacts.common import PickleArtifact
+from bentoml.handlers import DataframeHandler
+from bentoml.adapters import DataframeInput
+
+@bentoml.artifacts([PickleArtifact('model')])
+@bentoml.env(pip_packages=["scikit-learn", "pandas"])
+class ChurnPredictionModel(bentoml.BentoService):
+
+    @bentoml.api(input=DataframeInput(), batch=True)
+    def predict(self, df):
+        """
+        predict expects pandas.Series as input
+        """        
+#         series = df.iloc[0,:]
+        return self.artifacts.model.predict(df)
